ab29f2a0a86ab0f1244323ae6a393b5d446f11d8
Liran Tal <liran.tal@gmail.com>
2019-08-14 12:26:08 +0300

    feat: handle file uploads

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 "use strict";
 const PORT = 3112;
+const FILE_OUTPUT = "/tmp/resized_picture.jpg";
 
 const path = require("path");
+const fs = require("fs");
+const pump = require("pump");
+const fastifyMultipart = require("fastify-multipart");
 const fastifyStatic = require("fastify-static");
 const fastify = require("fastify")({
   logger: {
@@ -10,11 +14,30 @@ const fastify = require("fastify")({
   }
 });
 
+function uploadFileHandler(field, file, filename, encoding, mimetype) {
+  fs.unlink(FILE_OUTPUT, () => {
+    pump(file, fs.createWriteStream(FILE_OUTPUT));
+  });
+}
+
+fastify.post("/upload", (req, res) => {
+  req.multipart(uploadFileHandler, err => {
+    if (err) {
+      fastify.log.error(err);
+    }
+
+    fastify.log.info("upload completed");
+    fastify.log.info("commencing image resizing");
+  });
+});
+
+fastify.register(fastifyMultipart);
+
 fastify.register(fastifyStatic, {
   root: path.join(__dirname, "public")
 });
 
-fastify.listen(PORT, err => {
+fastify.listen(PORT, "0.0.0.0", err => {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
