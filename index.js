c85cd646a35547d6f8915375994347b2dab7cf05
Liran Tal <liran.tal@gmail.com>
2019-08-14 12:27:03 +0300

    feat: use convert to resize the image

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 "use strict";
 const PORT = 3112;
 const FILE_OUTPUT = "/tmp/resized_picture.jpg";
+const FILE_RESULT = "public/result.jpg";
 
 const path = require("path");
 const fs = require("fs");
+const child_process = require("child_process");
+
 const pump = require("pump");
 const fastifyMultipart = require("fastify-multipart");
 const fastifyStatic = require("fastify-static");
@@ -28,6 +31,14 @@ fastify.post("/upload", (req, res) => {
 
     fastify.log.info("upload completed");
     fastify.log.info("commencing image resizing");
+
+    child_process.execFile(
+      "/usr/bin/convert",
+      [FILE_OUTPUT, "-resize", "280x150", FILE_RESULT],
+      () => {
+        res.redirect("/result.html");
+      }
+    );
   });
 });
 
